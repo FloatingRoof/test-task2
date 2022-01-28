9b17017aa74ffe1471a0e2d16fe6f9d940259496
Денис <denisroof@mail.ru>
2022-01-28 23:09:02 +0300

    add Search

--- src/App.js
+++ src/App.js
@@ -11,31 +11,32 @@ import {Settings} from "./features/settings/components/Settings";
 import {KnowledgeBase} from "./features/knowledge-base/components/Knowledge-base";
 import {Employees} from "./features/employees/components/Employees";
 import {Assets} from "./features/assets/components/Assets";
+import {Search} from "./features/header/components/Search";
 
 function App() {
     return (
         <BrowserRouter>
             <Provider store={store}>
-        <div className="App">
-            <div className="App__header-block">
-                <Header/>
+                <div className="App">
+                    <div className="App__header-block">
+                        <Header/>
 
-            </div>
-            <div className="App__content-block">
-                <div className="App__search">
-dsadas
+                    </div>
+                    <div className="App__content-block">
+                        <div className="App__search">
+                            <Search/>
+                        </div>
+                        <Routes>
+                            <Route path="/applications" element={<Applications/>}/>
+                            <Route path="/clients" element={<Clients/>}/>
+                            <Route path="/settings" element={<Settings/>}/>
+                            <Route path="/knowledge-base" element={<KnowledgeBase/>}/>
+                            <Route path="/employees" element={<Employees/>}/>
+                            <Route path="/assets" element={<Assets/>}/>
+                        </Routes>
+                    </div>
+
                 </div>
-                <Routes>
-                    <Route path="/applications" element={<Applications />}/>
-                    <Route path="/clients" element={<Clients />}/>
-                    <Route path="/settings" element={<Settings />}/>
-                    <Route path="/knowledge-base" element={<KnowledgeBase />}/>
-                    <Route path="/employees" element={<Employees />}/>
-                    <Route path="/assets" element={<Assets />}/>
-                </Routes>
-            </div>
-
-        </div>
 
             </Provider>
         </BrowserRouter>
--- src/features/header/components/Search.jsx
+++ src/features/header/components/Search.jsx
@@ -1,7 +1,27 @@
+import classes from "./Search.module.css"
+import { Field, Form, Formik} from "formik";
+import React from "react";
+import search from  "../../../assets/images/Search.png"
 
-export const Search = (props) =>{
-    return(
-        <>
-        </>
+export const Search = (props) => {
+
+
+    const initialValues = {
+        search: '',
+    }
+    return (
+        <div className={classes.search}>
+            <Formik initialValues={initialValues}>
+                {formik => {
+                    return (
+                        <Form className={classes.form}>
+                            <div className={classes.field}>
+                                <Field id="search" name="search"  />
+                                <button className={classes.buttonSearch} type='submit'><img src={search}/></button>
+                            </div>
+                        </Form>)
+                }}
+            </Formik>
+        </div>
     )
 }
